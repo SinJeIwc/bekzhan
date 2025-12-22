19c3ee13bffe6bbfc68feb686c97a1595913a897
Bekzhan <hp.arstanaliev@gmail.com>
2025-12-23 03:38:38 +0600

    Refactor layout and component structure for improved readability and consistency
    - Updated layout.tsx to enhance formatting and maintain consistent indentation.
    - Refactored page.tsx for better readability by aligning JSX elements.
    - Improved header.tsx formatting for clarity and consistency.
    - Cleaned up desktop-menu.tsx and mobile-menu.tsx for better structure and readability.
    - Enhanced theme-button.tsx and theme-provider.tsx for consistent formatting.
    - Refactored badge.tsx and button.tsx for improved readability and structure.
    - Updated dropdown-menu.tsx to enhance formatting and maintain consistent indentation.

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,18 +1,18 @@
 export default function Home() {
-	return (
-		<main className="container flex flex-col items-center justify-between min-h-screen mx-auto">
-			<div id="home" className="h-screen">
-				home
-			</div>
-			<div id="about" className="h-screen">
-				about
-			</div>
-			<div id="skills" className="h-screen">
-				skills
-			</div>
-			<div id="contact" className="h-screen">
-				contact
-			</div>
-		</main>
-	);
+  return (
+    <main className="container flex flex-col items-center justify-between min-h-screen mx-auto">
+      <div id="home" className="h-screen">
+        home
+      </div>
+      <div id="about" className="h-screen">
+        about
+      </div>
+      <div id="skills" className="h-screen">
+        skills
+      </div>
+      <div id="contact" className="h-screen">
+        contact
+      </div>
+    </main>
+  );
 }
--- client/src/components/navigation/mobile-menu.tsx
+++ client/src/components/navigation/mobile-menu.tsx
@@ -1,53 +1,67 @@
 import { Button } from "@components/ui/button";
 import {
-	Sheet,
-	SheetClose,
-	SheetContent,
-	SheetDescription,
-	SheetFooter,
-	SheetHeader,
-	SheetTitle,
-	SheetTrigger,
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
 } from "@components/ui/sheet";
 import { navigation } from "@constants/navigation";
 import { MenuIcon } from "lucide-react";
 import Link from "next/link";
 
 export function MobileMenu() {
-	return (
-		<Sheet>
-			<SheetTrigger asChild>
-				<Button variant="ghost" size="icon">
-					<MenuIcon className="w-6 h-6" />
-					<span className="sr-only">Open menu</span>
-				</Button>
-			</SheetTrigger>
-			<SheetContent>
-				<SheetHeader>
-					<SheetTitle></SheetTitle>
-					<SheetDescription></SheetDescription>
-				</SheetHeader>
-				<nav className="grid flex-1 gap-6 auto-rows-min">
-					{navigation.map((item) => (
-						<SheetClose asChild key={item.title}>
-							<Link
-								href={item.url}
-								className="flex items-center gap-2 px-4 text-lg font-medium hover:underline"
-							>
-								<item.icon className="w-5 h-5" />
-								{item.title}
-							</Link>
-						</SheetClose>
-					))}
-				</nav>
-				<SheetFooter>
-					<SheetClose asChild>
-						<Button variant="secondary" className="w-full">
-							Close
-						</Button>
-					</SheetClose>
-				</SheetFooter>
-			</SheetContent>
-		</Sheet>
-	);
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="ghost" size="icon">
+          <MenuIcon className="w-6 h-6" />
+          <span className="sr-only">Open menu</span>
+        </Button>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle className="flex gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+            >
+              <title>Logo</title>
+              <path
+                fill="currentColor"
+                d="M2 2h20v5h-2.915l.385 3H22v2h-2.274l1.393 10.865l-1.984.254L17.71 12H6.258l-1.39 11.116l-1.984-.248L4.242 12H2v-2h2.492l.375-3H2zm4.883 5l-.375 3H11V7zM13 7v3h4.454l-.385-3z"
+              ></path>
+            </svg>
+            <span>Avenue</span>
+          </SheetTitle>
+          <SheetDescription>Continue the route</SheetDescription>
+        </SheetHeader>
+        <nav className="flex flex-col">
+          {navigation.map((item) => (
+            <SheetClose asChild key={item.title}>
+              <Link
+                href={item.url}
+                className="flex items-center gap-2 px-4 py-6 text-lg font-medium rounded-md hover:bg-chart-2/10 focus:outline-none focus:ring-1 focus:ring-chart-2"
+              >
+                <item.icon className="w-5 h-5" />
+                {item.title}
+              </Link>
+            </SheetClose>
+          ))}
+        </nav>
+        <SheetFooter>
+          <SheetClose asChild>
+            <Button variant="secondary" className="w-full">
+              Close
+            </Button>
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
+  );
 }
