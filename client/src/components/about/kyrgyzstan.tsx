a2a76ebceae25d1d3b7b77b46d530f543b726d58
Bekzhan <hp.arstanaliev@gmail.com>
2025-12-25 05:27:18 +0600

    feat: add Kyrgyzstan component with image popover and create Tooltip component with Radix UI

---
+++ client/src/components/about/kyrgyzstan.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import { Popover, PopoverContent, PopoverTrigger } from "@ui/popover";
+
+export function Kyrgyzstan() {
+	return (
+		<Popover>
+			<PopoverTrigger className="underline text-chart-2">
+				Kyrgyzstan
+			</PopoverTrigger>
+			<PopoverContent asChild className="p-0" side="top">
+				<Image src="/kg.jpg" alt="Kyrgyzstan" width={200} height={200} />
+			</PopoverContent>
+		</Popover>
+	);
+}
